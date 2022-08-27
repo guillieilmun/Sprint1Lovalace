41352d6040deb7a110f393d6b8dc850766653947
guillieilmun <guillermobecerra92@yahoo.es>
2022-08-26 22:50:31 -0500

    Este es aporte empresa

---
+++ empresa.java
@@ -0,0 +1,24 @@
+public class empresa {
+
+    //Atributos
+    private int nitEmpresa;
+    private String nombreEmpresa;
+    private String tipoEmpresa;
+    private String ciucadEmpresa;
+    private String direccionEmpresa;
+    private String telefonoEmpresa;
+    private String correoEmpresa;
+
+    //Método Constructor
+    public Empresa() {
+    }
+    public Empresa(int nitempresa, String nombreEmpresa, String tipoEmpresa, String ciudadEmpresa, String direccionEmpresa, String telefonoEmpresa, String correoEmpresa) {
+        this.nitEmpesa = nitEmpresa;
+        this.nombreEmpresa = nombreEmpresa;
+        this tipoEmpresa = tipoEmpresa;
+        this .ciudadEmpresa = ciudadEmpresa;
+        this.direccionEmpresa = oireccionEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.correoEmpresa = correoEmpresa;
+
+        // Get an seter
