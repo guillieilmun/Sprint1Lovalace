a378fda54cba7bd61b26da98a0282a619a7b3ded
carediher <carediher25@gmail.com>
2022-08-28 21:26:10 -0500

    Scrum Master: Corrección Movimiento Dinero

--- MovimientoDinero/src/main/java/MovimientoDinero.java
+++ MovimientoDinero/src/main/java/MovimientoDinero.java
@@ -1,107 +1,53 @@
 public class MovimientoDinero {
     /* Atributos */
-        private String ventaContado;
-        private String pagoDeudores;
-        private String ventasCredito;
-        private String prestamosObtenidos;
-        private String compraMateriaprima;
-        private String pagoProvehedores;
-        private String pagoNomina;
-        private String pagoPrestamos;
-        private String compraInsumos;
+    private String montoMovomiento;
+    private String concptoMovimiento;
+    private String encargadoMovimiento;
+    private String montoPoNe;
 
-        /* Constructores*/
+    /* Constructores*/
 
-        public MovimientoDinero() {
-        }
+    public MovimientoDinero() {
+    }
 
-        public MovimientoDinero(String ventaContado, String pagoDeudores, String ventasCredito, String prestamosObtenidos, String compraMateriaprima, String pagoProvehedores, String pagoomina, String pagoPrestamos, String compraInsumos) {
-            this.ventaContado = ventaContado;
-            this.pagoDeudores = pagoDeudores;
-            this.ventasCredito = ventasCredito;
-            this.prestamosObtenidos = prestamosObtenidos;
-            this.compraMateriaprima = compraMateriaprima;
-            this.pagoProvehedores = pagoProvehedores;
-            this.pagoNomina = pagoomina;
-            this.pagoPrestamos = pagoPrestamos;
-            this.compraInsumos = compraInsumos;
-        }
+    public MovimientoDinero(String montoMovomiento, String concptoMovimiento, String encargadoMovimiento, String montoPoNe) {
+        this.montoMovomiento = montoMovomiento;
+        this.concptoMovimiento = concptoMovimiento;
+        this.encargadoMovimiento = encargadoMovimiento;
+        this.montoPoNe = montoPoNe;
+    }
 
-        // Getter and Setter
+    // Getter and Setter
 
+    public String getMontoMovomiento() {
+        return montoMovomiento;
+    }
 
-        public String getVentaContado() {
-            return ventaContado;
-        }
+    public void setMontoMovomiento(String montoMovomiento) {
+        this.montoMovomiento = montoMovomiento;
+    }
 
-        public void setVentaContado(String ventaContado) {
-            this.ventaContado = ventaContado;
-        }
+    public String getConcptoMovimiento() {
+        return concptoMovimiento;
+    }
 
-        public String getPagoDeudores() {
-            return pagoDeudores;
-        }
+    public void setConcptoMovimiento(String concptoMovimiento) {
+        this.concptoMovimiento = concptoMovimiento;
+    }
 
-        public void setPagoDeudores(String pagoDeudores) {
-            this.pagoDeudores = pagoDeudores;
-        }
+    public String getEncargadoMovimiento() {
+        return encargadoMovimiento;
+    }
 
-        public String getVentasCredito() {
-            return ventasCredito;
-        }
+    public void setEncargadoMovimiento(String encargadoMovimiento) {
+        this.encargadoMovimiento = encargadoMovimiento;
+    }
 
-        public void setVentasCredito(String ventasCredito) {
-            this.ventasCredito = ventasCredito;
-        }
+    public String getMontoPoNe() {
+        return montoPoNe;
+    }
 
-        public String getPrestamosObtenidos() {
-            return prestamosObtenidos;
-        }
-
-        public void setPrestamosObtenidos(String prestamosObtenidos) {
-            this.prestamosObtenidos = prestamosObtenidos;
-        }
-
-        public String getCompraMateriaprima() {
-            return compraMateriaprima;
-        }
-
-        public void setCompraMateriaprima(String compraMateriaprima) {
-            this.compraMateriaprima = compraMateriaprima;
-        }
-
-        public String getPagoProvehedores() {
-            return pagoProvehedores;
-        }
-
-        public void setPagoProvehedores(String pagoProvehedores) {
-            this.pagoProvehedores = pagoProvehedores;
-        }
-
-        public String getPagoNomina() {
-            return pagoNomina;
-        }
-
-        public void setPagoNomina(String pagoNomina) {
-            this.pagoNomina = pagoNomina;
-        }
-
-        public String getPagoPrestamos() {
-            return pagoPrestamos;
-        }
-
-        public void setPagoPrestamos(String pagoPrestamos) {
-            this.pagoPrestamos = pagoPrestamos;
-        }
-
-        public String getCompraInsumos() {
-            return compraInsumos;
-        }
-
-        public void setCompraInsumos(String compraInsumos) {
-            this.compraInsumos = compraInsumos;
-        }
-
-
-
+    public void setMontoPoNe(String montoPoNe) {
+        this.montoPoNe = montoPoNe;
+    }
 }
